fe64d9713f6fe114051224c578e82bde853c3fb7
KingFucktard <31320949+KingFucktard@users.noreply.github.com>
2017-09-16 15:57:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+//change the text
 var element = document.getElementById('main-text');
 element.innerHTML='sab kuch naya';
+//move the image
+var img= document.getElementById('madi');
+img.onclick =function() {
+    img.style.marginLeft='100px';
+}
