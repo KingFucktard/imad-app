da09d73a89349de38ce95537a0f39c4ec56326cd
KingFucktard <31320949+KingFucktard@users.noreply.github.com>
2017-09-16 15:19:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var articleName = res.params.articleName;
+
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
  res.send(createTemplate(articles(articleName)));
