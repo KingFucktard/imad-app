3ea5a94b08bcf77c1e0a536ce5c67955713cf64d
KingFucktard <31320949+KingFucktard@users.noreply.github.com>
2017-09-16 14:15:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article-three', function (req, res) {
   res.send('Article three requested and will be served here');
 });
 app.get('/article-two', function (req, res) {
-  res.send('Article two requested and will be served here');
+  res.sendFile(path.join(_dirname,'/','article-two.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
