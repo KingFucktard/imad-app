ba20174ee02422e48da7ac5f5bc77120a603e959
KingFucktard <31320949+KingFucktard@users.noreply.github.com>
2017-09-16 21:02:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,8 +40,7 @@ button.onclick=function(){
     
 };
 //submit name
-var nameInput=document.getElementById('name');
-var name = nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick= function(){
     var request = new XMLHttpRequest();
@@ -64,6 +63,8 @@ submit.onclick= function(){
         //not done
     };
     //make request
+    var nameInput=document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://albarvasi21.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
     
