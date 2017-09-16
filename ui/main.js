aaf13ab599ff3e65b585aa60e758b6e2792cfbc8
KingFucktard <31320949+KingFucktard@users.noreply.github.com>
2017-09-16 16:32:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,6 +21,7 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick=function(){
     //make arequest to countr endpoint
+    var request = new XMLHttpRequest();
     //capture the response time and store it in a variable
     //render the variable in correct span
     counter = counter +1;
