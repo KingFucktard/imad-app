a089f5cadebf0936ed5a2a6d5839278d6f3ddf67
KingFucktard <31320949+KingFucktard@users.noreply.github.com>
2017-09-16 19:54:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,3 +39,17 @@ button.onclick=function(){
     request.send(null);
     
 };
+//submit name
+var nameInput=document.geetElemenntById('name');
+var name = nameInput.value;
+var submit=document.geetElemenntById('submit_btn');
+submit.onclick= function(){
+  //makearequestto the server and send the name capture a nnamelist and render it as the list  
+  var names=['name1','name2','name3'];
+  var list =0;
+  for(var i=0;i<names.length;i++){
+      list +='<li>'+names[i]+ '</li>';
+  }
+  var ul=document.getElementById('namelist');
+  ul.innerHTML=list;
+};
